3429da32233978395f4c4eb33c9a27cf4b46e78a
Francisco Correa <fcorrea.fernandez@gmail.com>
2021-04-26 16:57:39 -0400

    fixing counter in seconds and adding more slots

---
+++ simple-counter/src/Counter.js
@@ -0,0 +1,68 @@
+import React, {useEffect, useState } from "react";
+import propTypes from "prop-types";
+import "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/free-solid-svg-icons";
+import "@fortawesome/react-fontawesome";
+import "bootstrap/dist/css/bootstrap.css";
+import "./css/App.css";
+
+const Counter = () => {
+  
+  const [counting, setCounting] = useState(0);
+  
+  useEffect(() => {
+      const interval = setInterval(() => {
+          setCounting(counting => counting + 1)
+      }, 1000);
+      return () => clearInterval(interval);
+  }, []);
+
+  const sixth = Math.floor(counting / 1 % 10)
+  const fifth = Math.floor(counting / 10 % 10)
+  const fourth = Math.floor(counting / 100 % 10)
+  const third = Math.floor(counting / 1000 % 10)
+  const second = Math.floor(counting / 10000 % 10)
+  const first = Math.floor(counting / 100000 % 10)
+  console.log(first, second, third, fourth, fifth, sixth, "<--counting in seconds")
+
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-1">
+          <div className="simpleCounter">
+            <i className="fa fa-clock-o"></i>
+          </div>
+        </div>
+        <div className="col-md-2">
+          <div className="seconds">{first}</div>
+        </div>
+        <div className="col-md-2">
+          <div className="seconds">{second}</div>
+        </div>
+        <div className="col-md-2">
+          <div className="seconds">{third}</div>
+        </div>
+        <div className="col-md-2">
+          <div className="seconds">{fourth}</div>
+        </div>
+        <div className="col-md-2">
+          <div className="seconds">{fifth}</div>
+        </div>
+        <div className="col-md-1">
+          <div className="seconds">{sixth}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Counter.propTypes = {
+  first: propTypes.number,
+  second: propTypes.number,
+  third: propTypes.number,
+  fourth: propTypes.number,
+  fifth: propTypes.number,
+  sixth: propTypes.number
+};
+
+export default Counter;
---
+++ simple-counter/src/Home.js
@@ -0,0 +1,13 @@
+import Counter from './Counter.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../src/css/App.css';
+
+function Home() {
+  return (
+    <div>
+        <Counter />
+    </div>
+  );
+}
+
+export default Home
